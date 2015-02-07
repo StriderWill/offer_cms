7d3c93cb97be06caa626bdb932f6d2484e0b81cd
Claire Williams <itsclairewilliams@gmail.com>
2015-02-07 15:01:38 +0100

    added validation for offer code

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,6 @@
 class Offer < ActiveRecord::Base
- validates :code, uniqueness: true, presence: true
+ validates :code, uniqueness: true, presence: true,
+ 	format: { with: /\[with0-9A-Z]{8}$\z/,
+ 	message: "Offer code must be 8 characters using capital letters and numbers"}
  #validates :deadline, 
 end
